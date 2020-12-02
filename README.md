3398d2e48e7f7918d5c884ea73bdae650e692804
Haruki Miyagi <haruki.miyagi11@gmail.com>
2020-12-02 21:51:21 +0900

    Add 初期設定

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 ## 初期設定
 sh install.sh
-
-## 環境構築
--
-
-
-### Q&A
-- iterm(hotkeyが効かない時)
-https://www.smartbowwow.com/2018/11/mojaveiterm2hot-key.html
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo 'Setting up Mac...'
+
+# install xcode
+if [[ "$(xcode-select -p)" == "" ]]; then
+  echo '\nInstalling Xcode '
+  xcode-select --install
+  echo 'Done'
+else
+  echo '\nXcode already installed.'
+fi
+
+# install homebrew
+if test ! $(which brew); then
+  echo 'install homebrew'
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo '\nhomebrew already installed.'
+fi
