8d4c4da129d409592eeadbf410ad33b6baf0f6ca
Haruki Miyagi <haruki.miyagi11@gmail.com>
2023-07-14 11:15:45 +0900

    m1 test

--- install.sh
+++ install.sh
@@ -1,67 +1,71 @@
 #!/bin/sh
 
-echo 'Setting up Mac...'
+# echo 'Setting up Mac...'
 
-# install homebrew
-if test ! $(which brew); then
-  echo 'install homebrew'
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  echo '\nhomebrew already installed.'
-fi
+# # install homebrew
+# if test ! $(which brew); then
+#   echo 'install homebrew'
+#   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# else
+#   echo '\nhomebrew already installed.'
+# fi
 
-# Update Homebrew recipes
-brew update
+# # Update Homebrew recipes
+# brew update
 
-# Install Packages
-brew install git
-brew install tig
-brew install rbenv
-brew install jq
-brew install --cask iterm2
-brew install --cask alfred
-# 画面リサイズツール
-brew install --cask spectacle
-brew install --cask clipy
-brew install --cask appcleaner
-brew install --cask google-chrome
-brew install --cask visual-studio-code
-brew install --cask atom
-brew install --cask slack
-brew install --cask docker
-brew install --cask deepl
-# brew install --cask homebrew/cask-versions/sequel-pro-nightly
-# MySQL・MariaDBに接続できるデータベースクライアントツール
-# brew install --cask tableplus
-# brew install --cask ngrok
-# brew install --cask recordit
-# brew install --cask tunnelblick
-# brew install --cask dropbox
-# brew install --cask libreoffice
-# brew install --cask lastpass
-# brew install --cask 1password
-# brew install --cask skitch
-# brew install --cask anki
-# brew install --cask insomnia
-# brew install --cask vivaldi
-# brew install --cask deckset
-# brew install --cask caffeine
-# ノイズキャンセリング
-# brew install --cask krisp
-# フォトショに似たアプリ
-# brew install --cask gimp
-# MacからWindowsへ文字化けしない圧縮ファイルを作成
-# brew install --cask macwinzipper
+# # Install Packages
+# brew install git
+# brew install tig
+# brew install rbenv
+# brew install jq
+# brew install watch
+# brew install --cask iterm2
+# brew install --cask alfred
+# # 画面リサイズツール
+# brew install --cask spectacle
+# brew install --cask clipy
+# brew install --cask appcleaner
+# brew install --cask google-chrome
+# brew install --cask visual-studio-code
+# brew install --cask atom
+# brew install --cask slack
+# brew install --cask docker
+# brew install --cask deepl
+# # brew install --cask homebrew/cask-versions/sequel-pro-nightly
+# # MySQL・MariaDBに接続できるデータベースクライアントツール
+# # brew install --cask tableplus
+# # brew install --cask ngrok
+# # brew install --cask recordit
+# # brew install --cask tunnelblick
+# # brew install --cask dropbox
+# # brew install --cask libreoffice
+# # brew install --cask lastpass
+# # brew install --cask 1password
+# # brew install --cask skitch
+# # brew install --cask anki
+# # brew install --cask insomnia
+# # brew install --cask vivaldi
+# # brew install --cask caffeine
+# # ノイズキャンセリング
+# # brew install --cask krisp
+# # フォトショに似たアプリ
+# # brew install --cask gimp
+# # MacからWindowsへ文字化けしない圧縮ファイルを作成
+# # brew install --cask macwinzipper
+# # デスクトップバーの非表示
+# # brew install --cask hiddenbar
 
-brew cleanup
-brew cask cleanup
+# brew cleanup
+# brew cask cleanup
 
-# git clone dotfile
-mkdir ~/src/github.com/hrk-m
-cd ~/src/github.com/hrk-m
-git clone git@github.com:hrk-m/dotfiles.git
+# # git clone dotfile
+# mkdir ~/src/github.com/hrk-m
+# cd ~/src/github.com/hrk-m
+# git clone git@github.com:hrk-m/dotfiles.git
 
-# Display hidden files / folders
-echo "Display hidden files / folders"
-defaults write com.apple.finder AppleShowAllFiles TRUE
-killall Finder
+# # Display hidden files / folders
+# echo "Display hidden files / folders"
+# defaults write com.apple.finder AppleShowAllFiles TRUE
+# killall Finder
+
+echo 'M1 test'
