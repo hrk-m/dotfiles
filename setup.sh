553ff2a374356c7a18fca1b12d51701530147194
Haruki Miyagi <haruki.miyagi11@gmail.com>
2023-07-19 13:07:38 +0900

    os設定を追加

--- setup.sh
+++ setup.sh
@@ -2,4 +2,17 @@
 USER=$(id -u)
 DOTFILES=$HOME/src/github.com/hrk-m/dotfiles
 
-mkdir -p "${HOME}"/.ssh
+echo "Set a blazingly fast keyboard repeat rate"
+defaults write -g KeyRepeat -int 1
+
+echo "Set a shorter Delay until key repeat"
+defaults write -g InitialKeyRepeat -int 17
+
+echo "Dockを右に"
+defaults write com.apple.dock "orientation" -string "right" && killall Dock
+
+echo "Finder 内の隠しファイルを表示する"
+defaults write com.apple.finder "AppleShowAllFiles" -bool "true" && killall Finder
+
+# パスバーを表示
+defaults write com.apple.finder "ShowPathbar" -bool "true" && killall Finder
